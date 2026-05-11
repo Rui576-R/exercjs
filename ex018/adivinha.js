6a9008aff1bd2d4ad5de5f0c82fb69cac293acef
ruiaparecido <82909004+Rui576-R@users.noreply.github.com>
2026-05-10 21:12:52 -0300

    Atualizar

---
+++ ex018/adivinha.js
@@ -0,0 +1,24 @@
+// Todas as variaveis aqui em cima - fora das funções - são consideradas GLOBAIS e funcionam dentro de qualquer outra função!
+let res = document.querySelector('section#resultado')
+let computador = 0
+let jogador = 0
+
+function sortear() {
+    let min = 1 // As variaveis declaradas aqui dentro são LOCAIS, e só funcionam dentro da função onde foram declaradas
+    let max = 100
+    let dif = max - min
+    let aleatorio = Math.random()
+    computador = min + Math.trunc(dif * aleatorio)
+}
+
+function jogar() {
+    jogador = Number(window.prompt('Qual é o seu palpite?'))
+    if (jogador < computador) {
+        res.innerHTML += `<p>Você falou ${jogador}. Meu número é <strong>MAIOR</strong>!</p>`
+    } else if (jogador > computador) {
+        res.innerHTML += `<p>Você falou ${jogador}. Meu número é <strong>MENOR</strong>!</p>`
+    } else if (jogador == computador) {
+        res.innerHTML += `<p><strong>PARABENS!</strong> Você acertou! Eu tinha sorteado o valor ${computador}!</p>`
+        document.getElementById('botao').style.visibility = 'hidden'
+    }
+}
